e4b1b5fd327ebe01bfba278d89d5c375435a49bd
Fabian Yate Ramirez <fabianyate31@gmail.com>
2023-04-16 16:51:45 -0500

    Add entry point to get all the accounts for a specific user

--- src/main/java/com/wallet/mono/controller/AccountController.java
+++ src/main/java/com/wallet/mono/controller/AccountController.java
@@ -1,14 +1,19 @@
 package com.wallet.mono.controller;
 
 import com.wallet.mono.domain.dto.AccountRequest;
+import com.wallet.mono.domain.dto.AccountResponse;
 import com.wallet.mono.service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -20,6 +25,11 @@ public class AccountController {
     public ResponseEntity<Void> saveUser(@RequestBody AccountRequest accountRequest) throws Exception {
         accountService.saveAccount(accountRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("user/{userId}")
+    public ResponseEntity<List<AccountResponse>> getAccountsByUserId(@PathVariable("userId") Integer userId) throws Exception {
+        return new ResponseEntity<>(accountService.getAccountsByUserId(userId), HttpStatus.OK);
     }
 
 }
