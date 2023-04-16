a7e30c3c0141cc34b40fda1afe1d53dc57997572
Fabian Yate Ramirez <fabianyate31@gmail.com>
2023-04-16 16:53:06 -0500

    Add account repository

--- src/main/java/com/wallet/mono/repository/AccountRepository.java
+++ src/main/java/com/wallet/mono/repository/AccountRepository.java
@@ -3,6 +3,9 @@ package com.wallet.mono.repository;
 import com.wallet.mono.domain.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    List<Account> findByUser_UserId(Integer userId);
     boolean existsByAccountName(String accountName);
 }
