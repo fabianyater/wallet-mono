0e6753e4cf4b956c650bc56a1b8a32a5063f6cbc
Fabian Yate Ramirez <fabianyate31@gmail.com>
2023-04-16 16:52:38 -0500

    Add new service method

--- src/main/java/com/wallet/mono/service/AccountService.java
+++ src/main/java/com/wallet/mono/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.wallet.mono.service;
 
 import com.wallet.mono.domain.dto.AccountRequest;
+import com.wallet.mono.domain.dto.AccountResponse;
+
+import java.util.List;
 
 public interface AccountService {
     void saveAccount(AccountRequest accountRequest) throws Exception;
+    public List<AccountResponse> getAccountsByUserId(int userId) throws Exception;
 
 }
--- src/main/java/com/wallet/mono/service/serviceImpl/AccountServiceImpl.java
+++ src/main/java/com/wallet/mono/service/serviceImpl/AccountServiceImpl.java
@@ -1,7 +1,9 @@
 package com.wallet.mono.service.serviceImpl;
 
 import com.wallet.mono.domain.dto.AccountRequest;
+import com.wallet.mono.domain.dto.AccountResponse;
 import com.wallet.mono.domain.mapper.AccountRequestMapper;
+import com.wallet.mono.domain.mapper.AccountResponseMapper;
 import com.wallet.mono.domain.model.Account;
 import com.wallet.mono.exception.AccountAlreadyExistsException;
 import com.wallet.mono.exception.UserNotFoundException;
@@ -12,6 +14,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @AllArgsConstructor
 @Transactional
@@ -20,6 +23,7 @@ public class AccountServiceImpl implements AccountService {
     private final UserService userService;
     private final AccountRepository accountRepository;
     private final AccountRequestMapper accountRequestMapper;
+    private final AccountResponseMapper accountResponseMapper;
 
     @Override
     public void saveAccount(AccountRequest accountRequest) throws Exception {
@@ -27,12 +31,29 @@ public class AccountServiceImpl implements AccountService {
             throw new UserNotFoundException();
         }
 
-        if (accountRepository.existsByAccountName(accountRequest.getAccountName())){
+        List<AccountResponse> accounts = getAccountsByUserId(Integer.parseInt(accountRequest.getUserId()));
+
+        if (accountNameAlreadyExists(accounts, accountRequest.getAccountName())){
             throw new AccountAlreadyExistsException();
         }
 
         Account account = accountRequestMapper.mapToAccount(accountRequest);
         accountRepository.save(account);
 
+    }
+
+    @Override
+    public List<AccountResponse> getAccountsByUserId(int userId) throws Exception {
+        if (!userService.existsUser(userId)) {
+            throw new UserNotFoundException();
+        }
+
+        List<Account> accounts = accountRepository.findByUser_UserId(userId);
+
+        return accountResponseMapper.mapToAccountResponseList(accounts);
+    }
+
+    private boolean accountNameAlreadyExists(List<AccountResponse> accountResponses, String name) {
+        return accountResponses.stream().anyMatch(accountResponse -> accountResponse.getAccountName().equals(name));
     }
 }
