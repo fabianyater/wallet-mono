a634d66c9afe684425422d8fe9beb7059ccc159f
Fabian Yate Ramirez <fabianyate31@gmail.com>
2023-04-16 16:52:07 -0500

    Add mapper and dto for account

---
+++ src/main/java/com/wallet/mono/domain/dto/AccountResponse.java
@@ -0,0 +1,12 @@
+package com.wallet.mono.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AccountResponse {
+    private String accountName;
+    private String accountCurrency;
+    private Double accountBalance;
+}
---
+++ src/main/java/com/wallet/mono/domain/mapper/AccountResponseMapper.java
@@ -0,0 +1,17 @@
+package com.wallet.mono.domain.mapper;
+
+import com.wallet.mono.domain.dto.AccountResponse;
+import com.wallet.mono.domain.model.Account;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AccountResponseMapper {
+    AccountResponse mapToAccountResponse(Account account);
+
+    List<AccountResponse> mapToAccountResponseList(List<Account> accounts);
+}
