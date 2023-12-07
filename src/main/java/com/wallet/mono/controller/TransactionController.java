fdceb0b22856691af1fb787ccca7a2aa22835884
Fabian Yate <fabianyate31@gmail.com>
2023-12-06 20:25:10 -0500

    Add transition statistics

--- src/main/java/com/wallet/mono/controller/TransactionController.java
+++ src/main/java/com/wallet/mono/controller/TransactionController.java
@@ -1,17 +1,17 @@
 package com.wallet.mono.controller;
 
-import com.wallet.mono.domain.dto.ListTransactionResponse;
-import com.wallet.mono.domain.dto.TotalAmountResponse;
-import com.wallet.mono.domain.dto.TransactionRequest;
-import com.wallet.mono.domain.dto.TransactionResponse;
+import com.wallet.mono.domain.dto.*;
 import com.wallet.mono.service.TransactionService;
 import com.wallet.mono.utils.ApiResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -71,6 +71,22 @@ public class TransactionController {
     public ResponseEntity<TotalAmountResponse> getTotalTransactionsAmount(
             @PathVariable("accountId") Integer accountId) throws Exception {
         return new ResponseEntity<>(transactionService.getTotalIncomeByAccountId(accountId), HttpStatus.OK);
+    }
+
+    @GetMapping("stats/account/{accountId}")
+    public ResponseEntity<ApiResponse<List<TransactionStatistics>>> getStats(
+            @PathVariable("accountId") Integer accountId,
+            @PathParam("startDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+            @PathParam("endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) throws Exception {
+        List<TransactionStatistics> transactionStadistics = transactionService.getTransactionStatistics(accountId, startDate, endDate);
+        ApiResponse<List<TransactionStatistics>> apiResponse = new ApiResponse<>();
+
+        apiResponse.setData(transactionStadistics);
+        apiResponse.setStatus(HttpStatus.OK.value());
+        apiResponse.setMessage("Todo ok");
+        apiResponse.setAdditionalInfo(null);
+        apiResponse.setPagination(null);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
 
--- src/main/java/com/wallet/mono/domain/mapper/TransactionResponseMapper.java
+++ src/main/java/com/wallet/mono/domain/mapper/TransactionResponseMapper.java
@@ -1,13 +1,16 @@
 package com.wallet.mono.domain.mapper;
 
 import com.wallet.mono.domain.dto.TransactionResponse;
+import com.wallet.mono.domain.dto.TransactionStatistics;
 import com.wallet.mono.domain.model.Transaction;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.domain.Page;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",
         unmappedSourcePolicy = ReportingPolicy.IGNORE,
@@ -24,5 +27,24 @@ public interface TransactionResponseMapper {
     TransactionResponse mapToTransactionResponse(Transaction transaction);
 
     List<TransactionResponse> mapToTransactionResponseList(List<Transaction> transactions);
+
     List<TransactionResponse> mapToTransactionResponseList(Page<Transaction> transactions);
+
+
+    default TransactionStatistics map(Object[] object) {
+        TransactionStatistics statistics = new TransactionStatistics();
+        statistics.setDay((String) object[0]);
+        statistics.setIncome(((Number) object[1]).longValue());
+        statistics.setExpense(((Number) object[2]).longValue());
+        return statistics;
+    }
+
+    default List<TransactionStatistics> mapToTransactionStatistics(List<Object[]> objects) {
+        if (objects == null) {
+            return Collections.emptyList();
+        }
+        return objects.stream()
+                .map(this::map)
+                .collect(Collectors.toList());
+    }
 }
