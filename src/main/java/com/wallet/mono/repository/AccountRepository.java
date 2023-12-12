03bc90e3e42e4cadcb5aa30c8ff471e6a6757aec
Fabian Yate <fabianyate31@gmail.com>
2023-12-11 22:21:44 -0500

    Remove update methods from AccountRepository
    Two update methods: updateIsFavoriteByAccountId and updateAccountBalanceByAccountId, along with their annotations, have been removed from AccountRepository.java. Instead, a query method to sum balances from the related wallets is introduced. This change optimizes performance by limiting the number of update transactions and shifts towards accessing balance information from the associated wallets.

--- src/main/java/com/wallet/mono/repository/AccountRepository.java
+++ src/main/java/com/wallet/mono/repository/AccountRepository.java
@@ -2,27 +2,17 @@ package com.wallet.mono.repository;
 
 import com.wallet.mono.domain.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    @Transactional
-    @Modifying
-    @Query("update Account a set a.isFavorite = ?1 where a.accountId = ?2")
-    void updateIsFavoriteByAccountId(boolean isFavorite, Integer accountId);
 
-    @Transactional
-    @Modifying
-    @Query("update Account a set a.accountBalance = ?1 where a.accountId = ?2")
-    int updateAccountBalanceByAccountId(Double accountBalance, Integer accountId);
     Account findByAccountId(Integer accountId);
     Account findByAccountIdAndUser_UserId(Integer accountId, Integer userId);
     boolean existsByAccountId(Integer accountId);
     List<Account> findByUser_UserId(Integer userId);
-    @Query(value = "select a.account_balance from accounts a where a.id = :id", nativeQuery = true)
-    Double findAccountBalance(@Param("id") Integer id);
+    @Query(value = "select sum(w.balance) from wallets w where w.account_id = :accountId", nativeQuery = true)
+    Double findGeneralAccountBalance(@Param("accountId") Integer accountId);
 }
